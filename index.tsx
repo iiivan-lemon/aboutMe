03de628c5fd50718efc315bb96d33ea82abbbec3
Ivan <lipton2807@yandex.ru>
2023-08-07 14:53:03 +0300

    new(app): add page transition

--- index.tsx
+++ index.tsx
@@ -1,7 +1,78 @@
-import React from 'react'
-import  { createRoot }  from 'react-dom/client';
-import App from './src/App'
+import React from "react";
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-root.render(<App/>);
+import './src/App.sass'
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  useLocation,
+  useOutlet
+} from "react-router-dom";
+import {CSSTransition, SwitchTransition} from "react-transition-group";
+import {createRoot} from "react-dom/client";
+import Header from "./src/components/header/Header.component";
+import ProjectsPage from "./src/pages/projectsPage/ProjectsPage";
+import MainPage from "./src/pages/mainPage/MainPage";
+import ResumePage from "./src/pages/resumePage/ResumePage";
+
+
+const routes = [
+  {path: '/', name: 'MainPage', element: <MainPage/>, nodeRef: React.createRef()},
+  {path: '/resume', name: 'ResumePage', element: <ResumePage/>, nodeRef: React.createRef()},
+  {
+    path: '/projects',
+    name: 'Projects',
+    element: <ProjectsPage/>,
+    nodeRef: React.createRef(),
+  },
+]
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: routes.map((route) => ({
+      index: route.path === '/',
+      path: route.path === '/' ? undefined : route.path,
+      element: route.element,
+    })),
+  },
+])
+
+function App() {
+
+
+  const location = useLocation()
+  const currentOutlet = useOutlet()
+  const {nodeRef} : any =
+  routes.find((route) => route.path === location.pathname) ?? React.useRef(null)
+
+
+  return (
+    <>
+        <Header/>
+        <SwitchTransition>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef}
+            timeout={400}
+            classNames="page"
+            unmountOnExit
+          >
+            {(state) => (
+              <div ref={nodeRef} className="page">
+                {currentOutlet}
+              </div>
+            )}
+          </CSSTransition>
+        </SwitchTransition>
+    </>
+
+  )
+}
+
+const container = document.getElementById('root')
+const root = createRoot(container as HTMLElement)
+root.render(<RouterProvider router={router} />)
+
--- src/pages/resumePage/ResumePage.tsx
+++ src/pages/resumePage/ResumePage.tsx
@@ -4,9 +4,10 @@ import {CSSTransition} from "react-transition-group";
 function ResumePage() {
   const nodeRef = React.useRef(null);
   const [showResume, setShowResume] = React.useState(false)
-  const handleOnLoad = () => {
+  React.useEffect(()=>{
     setShowResume(true)
-  }
+  }, [nodeRef])
+
   return (
     <main>
       <div className='main-title'>
@@ -24,7 +25,7 @@ function ResumePage() {
         onEnter={()=>{setShowResume(true)}}
         onExited={()=>{setShowResume(false)}}
         >
-      <object ref={nodeRef} onLoad={()=> handleOnLoad} width="100%" height="1000" data={myFile} type="application/pdf"></object>
+        <object ref={nodeRef} width="100%" height="1000" data={myFile} type="application/pdf"  style={{background: 'transparent url(AnimatedLoading.gif) no-repeat center;'}}/>
       </CSSTransition>
     </main>
   )
